62af594518aa0371d3e78f5040d2aef43a27111e
akhils <s.akhil@ogesinfotech.com>
2026-04-05 11:05:38 +0530

    screens changes

--- app/src/main/java/com/paradox543/malankaraorthodoxliturgica/ui/screens/ChurchScreen.kt
+++ app/src/main/java/com/paradox543/malankaraorthodoxliturgica/ui/screens/ChurchScreen.kt
@@ -1,10 +1,121 @@
 package com.paradox543.malankaraorthodoxliturgica.ui.screens
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import com.paradox543.malankaraorthodoxliturgica.ui.components.TopNavBar
 
 @Composable
 fun ChurchScreen(
     navController: NavController
-){
+) {
+
+    val isLoading = false
+    Scaffold(
+        topBar = { TopNavBar("Dioceses", navController) }) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(innerPadding)
+        ) { }
+        if (isLoading) {
+            Box(
+                modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        } else {
+            Column {
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize(),
+                    contentPadding = innerPadding
+                ) {
+                    /*items(diocesesData) { data ->
+                        PlaceCard(data)
+                    }*/
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ChurchCard() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp),
+        shape = RoundedCornerShape(16.dp),
+        elevation = CardDefaults.cardElevation(0.dp),
+        colors = CardDefaults.cardColors(contentColor = Color.White)
+    ) {
+        Column {
+            AsyncImage(
+                model = "",
+                contentDescription = "",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .clip(RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp))
+            )
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+
+                Text(
+                    text = "",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black,
+                    modifier = Modifier.padding(bottom = 8.dp)
+                )
+                Text(
+                    text = buildAnnotatedString {
+                        append(
+                            "description".take(100) + "... "
+                        )
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Black,
+                                fontWeight = FontWeight.Medium
+                            )
+                        ) {
+                            append("Read more")
+                        }
+                    },
+                    fontSize = 14.sp,
+                    color = Color.Black,
+                    lineHeight = 20.sp,
+                    modifier = Modifier.clickable {})
+            }
+        }
+    }
 }
