91c87ca07f2c15b5237b5660b1513fa6fddf7a36
Akhil <akhils.ogesinfotech@gmail.com>
2026-04-20 16:00:45 +0530

    display api req in log

--- data/calendar/src/main/java/com/paradox543/malankaraorthodoxliturgica/data/calendar/remote/KtorClient.kt
+++ data/calendar/src/main/java/com/paradox543/malankaraorthodoxliturgica/data/calendar/remote/KtorClient.kt
@@ -26,6 +26,7 @@ object KtorClient {
         install(Logging) {
             logger = Logger.ANDROID // prints to Android Logcat
             level = LogLevel.ALL // Changed from NONE to ALL to see logs
+
         }
         install(HttpTimeout) {
             requestTimeoutMillis = 60_000
